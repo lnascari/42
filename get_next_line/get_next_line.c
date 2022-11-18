99e7e4e6f68d2a9670db84f7d3fb1c2008c4ef7c
lnascari <lnascari@student.42firenze.it>
2022-11-18 15:37:09 +0100

    ok

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,8 +6,91 @@
 /*   By: lnascari <lnascari@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 13:20:37 by lnascari          #+#    #+#             */
-/*   Updated: 2022/11/10 21:48:31 by lnascari         ###   ########.fr       */
+/*   Updated: 2022/11/18 15:36:11 by lnascari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*get_next_line(int fd);
+#include <unistd.h>
+#include <stdlib.h>
+#include "get_next_line.h"
+
+static int	ft_len(char *s, int size)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\n' || s[i] != 0)
+	{
+		if (i == size)
+			return (-1);
+		i++;
+	}
+	return (i);
+}
+
+static void	ft_read(char **s, int fd)
+{
+	int	size;
+
+	size = BUFFER_SIZE;
+	*s = malloc(size + 1);
+	if (read(fd, s, size) == -1)
+		return (0);
+	while (ft_len(s, size) == -1)
+	{
+		free(*s);
+		size += BUFFER_SIZE;
+		*s = malloc(size + 1);
+		if (read(fd, *s, size) == -1)
+			return (0);
+	}
+	*s[size] = 0;
+}
+
+static int	ft_size(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\n')
+	{
+		if (s[i] == 0)
+			return (i * -1);
+		i++;
+	}
+	return (i);
+}
+
+static char	*get_line(char **s)
+{
+	int		size;
+	char	*line;
+
+	size = ft_size(*s);
+	if (size < 0)
+	{
+		line = malloc(size * -1 + 1);
+		ft_strlcpy(line, *s, size * -1 + 1);
+		free(*s);
+		*s = malloc(1);
+		s[0] = 0;
+	}
+	else
+	{
+		line = malloc(size + 2);
+		ft_strlcpy(line, *s, size + 2);
+		*s = *s + (size + 1);
+	}
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*s;
+
+	if (!s)
+		ft_read(&s, fd);
+	if (s[0] == 0 || (!s))
+		return (0);
+	return (get_line(&s));
+}
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,13 +6,17 @@
 /*   By: lnascari <lnascari@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 13:16:00 by lnascari          #+#    #+#             */
-/*   Updated: 2022/11/10 13:21:12 by lnascari         ###   ########.fr       */
+/*   Updated: 2022/11/18 15:21:50 by lnascari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
 char	*get_next_line(int fd);
+void	ft_strlcpy(char *dst, char *src, int size);
 
 #endif
