e2eb43f14ce17232134e6fa7eb17d83d59eb2b52
lnascari <lnascari@student.42firenze.it>
2023-01-19 14:28:13 +0100

    ok

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,12 +6,11 @@
 /*   By: lnascari <lnascari@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 16:02:07 by lnascari          #+#    #+#             */
-/*   Updated: 2023/01/19 12:46:16 by lnascari         ###   ########.fr       */
+/*   Updated: 2023/01/19 14:27:49 by lnascari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-#include <stdio.h>
 
 void	error(int *arr)
 {
@@ -68,11 +67,5 @@ int	main(int argc, char **argv)
 		while (--i > 0)
 			arr[j++] = check(argv[i], arr);
 		duplicates(arr, argc - 1);
-		i = -1;
-		while (++i < argc - 1)
-		{
-			printf("%d\n", arr[i]);
-		}
-		
 	}	
 }
