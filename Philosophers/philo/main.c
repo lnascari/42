9dd8d5d7744f68d310016034a436e9cad1c3f9fd
lnascari <lnascari@student.42firenze.it>
2023-03-01 11:49:07 +0100

    ok

--- Philosophers/philo/main.c
+++ Philosophers/philo/main.c
@@ -6,100 +6,79 @@
 /*   By: lnascari <lnascari@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 12:20:25 by lnascari          #+#    #+#             */
-/*   Updated: 2023/02/28 14:29:37 by lnascari         ###   ########.fr       */
+/*   Updated: 2023/03/01 11:43:45 by lnascari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+int	get_time(t_philo *philo)
+{
+	struct timeval	tv;
+
+	gettimeofday(&tv, 0);
+	return ((tv.tv_sec * 1000000 + tv.tv_usec - philo->info->time) / 1000);
+}
+
 void	sleep_death(t_philo *philo)
 {
-	long	time;
-
-	if (philo->info->time_to_eat
-		+ philo->info->time_to_sleep >= philo->info->time_to_die)
+	if (philo->info->tte + philo->info->tts > philo->info->ttd)
 	{
-		usleep((philo->info->time_to_die - philo->info->time_to_eat) * 1000);
-		time = philo->last_meal + philo->info->time_to_die * 1000;
+		usleep((philo->info->ttd - philo->info->tte) * 1000);
 		philo->info->death++;
 		if (philo->info->death == 1)
-			printf ("%ld\t%d is dead\n", time / 1000, philo->pos);
+			printf ("%d\t%d is dead\n", get_time(philo), philo->pos);
 	}
 }
 
-void	check_death(t_philo *philo)
+void	*check_death(void *arg)
 {
-	struct timeval	tv;
-	long			time;
-	long			ltime;
-	long			rtime;
-	long			maxtime;
+	t_info	*info;
+	int		i;
 
-	gettimeofday(&tv, 0);
-	time = tv.tv_sec * 1000000 + tv.tv_usec - philo->info->time;
-	ltime = *philo->llast_meal + philo->info->time_to_eat * 1000;
-	rtime = *philo->rlast_meal + philo->info->time_to_eat * 1000;
-	maxtime = rtime;
-	if (ltime > rtime)
-		maxtime = ltime;
-	if (maxtime > time)
+	info = (t_info *) arg;
+	while (!info->death)
 	{
-		if (maxtime > philo->last_meal + philo->info->time_to_die * 1000)
+		i = -1;
+		while (++i < info->nop)
 		{
-			maxtime = philo->last_meal + philo->info->time_to_die * 1000;
-			usleep(maxtime - time);
-			philo->info->death++;
-			if (philo->info->death == 1)
-				printf ("%ld\t%d is dead\n", maxtime / 1000, philo->pos);
+			if (info->philo[i].last_meal + info->ttd < get_time(info->philo))
+			{
+				info->death++;
+				if (info->death == 1)
+					printf ("%d\t%d is dead\n", get_time(info->philo),
+						info->philo[i].pos);
+			}
 		}
 	}
+	return (0);
 }
 
 void	*routine(void *arg)
 {
-	struct timeval	tv;
-	t_philo			*philo;
+	t_philo	*philo;
 
 	philo = (t_philo *) arg;
-	while (philo->number_of_times_each_philosopher_must_eat--)
+	while (philo->notepme-- && !philo->info->death)
 	{
-		gettimeofday(&tv, 0);
-		printf ("%ld\t%d is thinking\n", (tv.tv_sec * 1000000
-				+ tv.tv_usec - philo->info->time) / 1000, philo->pos);
-		check_death(philo);
-		if (philo->info->death)
-			return (0);
+		if (!philo->info->death)
+			printf ("%d\t%d is thinking\n", get_time(philo), philo->pos);
 		pthread_mutex_lock(&philo->rfork);
-		check_death(philo);
-		if (philo->info->death)
-			return (0);
-		gettimeofday(&tv, 0);
-		printf ("%ld\t%d has taken a fork\n", (tv.tv_sec * 1000000
-				+ tv.tv_usec - philo->info->time) / 1000, philo->pos);
+		if (!philo->info->death)
+			printf ("%d\t%d has taken a fork\n", get_time(philo), philo->pos);
 		pthread_mutex_lock(philo->lfork);
-		check_death(philo);
-		if (philo->info->death)
-			return (0);
-		gettimeofday(&tv, 0);
-		printf ("%ld\t%d has taken a fork\n", (tv.tv_sec * 1000000
-				+ tv.tv_usec - philo->info->time) / 1000, philo->pos);
-		gettimeofday(&tv, 0);
-		philo->last_meal = tv.tv_sec * 1000000 + tv.tv_usec - philo->info->time;
-		if (philo->info->death)
-			return (0);
-		printf ("%ld\t%d is eating\n", philo->last_meal / 1000, philo->pos);
-		usleep(philo->info->time_to_eat * 1000);
+		if (!philo->info->death)
+			printf ("%d\t%d has taken a fork\n", get_time(philo), philo->pos);
+		philo->last_meal = get_time(philo);
+		if (!philo->info->death)
+			printf ("%d\t%d is eating\n", philo->last_meal, philo->pos);
+		usleep(philo->info->tte * 1000);
 		pthread_mutex_unlock(&philo->rfork);
 		pthread_mutex_unlock(philo->lfork);
-		gettimeofday(&tv, 0);
-		if (philo->info->death)
-			return (0);
-		printf("%ld\t%d is sleeping\n", (tv.tv_sec * 1000000
-				+ tv.tv_usec - philo->info->time) / 1000, philo->pos);
+		if (!philo->info->death)
+			printf("%d\t%d is sleeping\n", get_time(philo), philo->pos);
 		sleep_death(philo);
-		usleep(philo->info->time_to_sleep * 1000);
-		if (philo->info->death)
-			return (0);
+		usleep(philo->info->tts * 1000);
 	}
 	return (0);
 }
@@ -107,64 +86,56 @@ void	*routine(void *arg)
 int	main(int argc, char **argv)
 {
 	struct timeval	tv;
-	t_philo			*philo;
 	t_info			info;
 	int				i;
 	int				n;
 
 	if (argc == 5 || argc == 6)
 	{
-		info.number_of_philosophers = ft_atoi(argv[1]);
-		info.time_to_die = ft_atoi(argv[2]);
-		info.time_to_eat = ft_atoi(argv[3]);
-		info.time_to_sleep = ft_atoi(argv[4]);
+		info.nop = ft_atoi(argv[1]);
+		info.ttd = ft_atoi(argv[2]);
+		info.tte = ft_atoi(argv[3]);
+		info.tts = ft_atoi(argv[4]);
 		if (argc == 6)
 			n = ft_atoi(argv[5]);
 		else
 			n = -1;
-		philo = malloc(sizeof(t_philo) * info.number_of_philosophers);
+		info.philo = malloc(sizeof(t_philo) * info.nop);
 		gettimeofday(&tv, 0);
 		info.time = tv.tv_sec * 1000000 + tv.tv_usec;
 		info.death = 0;
 		i = -1;
-		while (++i < info.number_of_philosophers)
+		while (++i < info.nop)
 		{
-			pthread_mutex_init(&philo[i].rfork, 0);
-			if (i == info.number_of_philosophers - 1)
-				philo[i].lfork = &philo[0].rfork;
+			pthread_mutex_init(&info.philo[i].rfork, 0);
+			if (i == info.nop - 1)
+				info.philo[i].lfork = &info.philo[0].rfork;
 			else
-				philo[i].lfork = &philo[i + 1].rfork;
-			if (i == info.number_of_philosophers - 1)
-				philo[i].llast_meal = &philo[0].last_meal;
-			else
-				philo[i].llast_meal = &philo[i + 1].last_meal;
-			if (i == 0)
-				philo[i].rlast_meal = &philo[info.number_of_philosophers
-					- 1].last_meal;
-			else
-				philo[i].rlast_meal = &philo[i - 1].last_meal;
-			philo[i].last_meal = 0;
-			philo[i].pos = i + 1;
-			philo[i].info = &info;
-			philo[i].number_of_times_each_philosopher_must_eat = n;
+				info.philo[i].lfork = &info.philo[i + 1].rfork;
+			info.philo[i].last_meal = 0;
+			info.philo[i].pos = i + 1;
+			info.philo[i].info = &info;
+			info.philo[i].notepme = n;
 		}
+		pthread_create(&info.dthread, 0, &check_death, &info);
 		i = 0;
-		while (i < info.number_of_philosophers)
+		while (i < info.nop)
 		{
-			pthread_create(&philo[i].thread, 0, &routine, philo + i);
+			pthread_create(&info.philo[i].thread, 0, &routine, info.philo + i);
 			i += 2;
 			usleep(1);
 		}
 		i = 1;
-		while (i < info.number_of_philosophers)
+		while (i < info.nop)
 		{
-			pthread_create(&philo[i].thread, 0, &routine, philo + i);
+			pthread_create(&info.philo[i].thread, 0, &routine, info.philo + i);
 			i += 2;
 			usleep(1);
 		}
+		pthread_join(info.dthread, 0);
 		i = -1;
-		while (++i < info.number_of_philosophers)
-			pthread_join(philo[i].thread, 0);
-		free(philo);
+		while (++i < info.nop)
+			pthread_join(info.philo[i].thread, 0);
+		free(info.philo);
 	}
 }
--- Philosophers/philo/philo.h
+++ Philosophers/philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: lnascari <lnascari@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 12:22:48 by lnascari          #+#    #+#             */
-/*   Updated: 2023/02/27 13:48:53 by lnascari         ###   ########.fr       */
+/*   Updated: 2023/03/01 11:24:46 by lnascari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,14 @@
 
 typedef struct s_info
 {
-	int				number_of_philosophers;
-	int				time_to_die;
-	int				time_to_eat;
-	int				time_to_sleep;
+	pthread_t		dthread;
+	int				nop;
+	int				ttd;
+	int				tte;
+	int				tts;
 	int				death;
 	long			time;
+	struct s_philo	*philo;
 }	t_info;
 
 typedef struct s_philo
@@ -33,11 +35,9 @@ typedef struct s_philo
 	pthread_mutex_t	rfork;
 	pthread_mutex_t	*lfork;
 	pthread_t		thread;
-	long			last_meal;
-	long			*rlast_meal;
-	long			*llast_meal;
+	int				last_meal;
 	int				pos;
-	int				number_of_times_each_philosopher_must_eat;
+	int				notepme;
 	t_info			*info;
 }	t_philo;
 
