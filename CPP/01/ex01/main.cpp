00c1e7b242b11e8146978276048df4ab9ce68265
user <lorenzonascari@gmail.com>
2023-06-27 23:16:29 +0200

    ok

--- CPP/01/ex01/main.cpp
+++ CPP/01/ex01/main.cpp
@@ -4,5 +4,8 @@ Zombie* zombieHorde(int N, std::string name);
 
 int main()
 {
-	
+	Zombie *horde = zombieHorde(5, "pippo");
+    for (int i = 0; i < 5; i++)
+        horde[i].announce();
+    delete[] horde;
 }
--- CPP/01/ex01/zombieHorde.cpp
+++ CPP/01/ex01/zombieHorde.cpp
@@ -2,5 +2,8 @@
 
 Zombie* zombieHorde(int N, std::string name)
 {
-	
+	Zombie* arr = static_cast<Zombie*>(::operator new[](N*sizeof(Zombie)));
+    for (size_t i = 0; i < N; i++)
+        ::new (arr+i) Zombie(name);
+    return arr;
 }
