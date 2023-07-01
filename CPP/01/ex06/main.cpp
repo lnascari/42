5a7fe48d8c1cf7d1a8d8f4e97fcbd85a7bd0aecf
user <lorenzonascari@gmail.com>
2023-07-01 09:59:40 +0200

    ok

--- CPP/01/ex06/main.cpp
+++ CPP/01/ex06/main.cpp
@@ -2,15 +2,31 @@
 
 int main(int argc, char const *argv[])
 {
+    if (argc != 2)
+        return 1;
     Harl harl;
-    switch (argv[1])
-    {
-    case "":
-        
-        harl.complain("");
+    std::string s[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+    int c = -1;
+    for (size_t i = 0; i < 4; i++) {
+        if (argv[1] == s[i])
+            c = i;
+    }
+    switch (c) {
+    case 0:
+        std::cout << "[DEBUG]" << std::endl;
+        harl.complain("DEBUG");
+    case 1:
+        std::cout << "[INFO]" << std::endl;
+        harl.complain("INFO");
+    case 2:
+        std::cout << "[WARNING]" << std::endl;
+        harl.complain("WARNING");
+    case 3:
+        std::cout << "[ERROR]" << std::endl;
+        harl.complain("ERROR");
         break;
-    
     default:
+        std::cout << "[DEFAULT]" << std::endl;
         break;
     }
     return 0;
