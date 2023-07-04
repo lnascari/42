5e53c1e3f2214c2fb1becf580efb1615749f0869
lnascari <lnascari@student.42firenze.it>
2023-07-04 14:54:52 +0200

    ok

--- CPP/02/ex00/Fixed.cpp
+++ CPP/02/ex00/Fixed.cpp
@@ -2,22 +2,38 @@
 
 Fixed::Fixed()
 {
+	std::cout << "Default constructor called" << std::endl;
 	n = 0;
 }
 
 Fixed::Fixed(const Fixed &f)
 {
-	n = f.n;
+	std::cout << "Copy constructor called" << std::endl;
+	*this = f;
 }
 
 Fixed &Fixed::operator=(const Fixed &f)
 {
+	std::cout << "Copy assignment operator called" << std::endl;
 	if(this == &f)
        return *this;
-	n = f.n;
+	n = f.getRawBits();
 	return *this;
 }
 
 Fixed::~Fixed()
 {
+	std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return n;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	n = raw;
 }
--- CPP/02/ex00/Fixed.hpp
+++ CPP/02/ex00/Fixed.hpp
@@ -13,6 +13,8 @@ public:
 	Fixed(const Fixed& f);
 	Fixed& operator=(const Fixed& f);
 	~Fixed();
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
 };
 
 #endif
