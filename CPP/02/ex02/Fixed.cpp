371eaabc22f18724b6d9ec0d1f42e0063d8aa5d1
lnascari <lnascari@student.42firenze.it>
2023-07-11 13:11:04 +0200

    ok

---
+++ CPP/02/ex02/Fixed.cpp
@@ -0,0 +1,127 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called" << std::endl;
+	n = 0;
+}
+
+Fixed::Fixed(const int n)
+{
+	std::cout << "Int constructor called" << std::endl;
+	this->n = n << 8;
+}
+
+Fixed::Fixed(const float n)
+{
+	std::cout << "Float constructor called" << std::endl;
+	this->n = roundf(n * 256);
+}
+
+Fixed::Fixed(const Fixed &f)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = f;
+}
+
+Fixed &Fixed::operator=(const Fixed &f)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if(this == &f)
+       return *this;
+	n = f.n;
+	return *this;
+}
+
+bool &Fixed::operator>(const Fixed &f) const
+{
+	bool b = n > f.n;
+	return b;
+}
+
+bool &Fixed::operator<(const Fixed &f) const
+{
+	bool b = n < f.n;
+	return b;
+}
+
+bool &Fixed::operator>=(const Fixed &f) const
+{
+	bool b = n >= f.n;
+	return b;
+}
+
+bool &Fixed::operator<=(const Fixed &f) const
+{
+	bool b = n <= f.n;
+	return b;
+}
+
+bool &Fixed::operator==(const Fixed &f) const
+{
+	bool b = n == f.n;
+	return b;
+}
+
+bool &Fixed::operator!=(const Fixed &f) const
+{
+	bool b = n != f.n;
+	return b;
+}
+
+Fixed &Fixed::operator+(const Fixed &f) const
+{
+	Fixed r(this->toFloat() + f.toFloat());
+	return r;
+}
+
+Fixed &Fixed::operator-(const Fixed &f) const
+{
+	Fixed r(this->toFloat() - f.toFloat());
+	return r;
+}
+
+Fixed &Fixed::operator*(const Fixed &f) const
+{
+	Fixed r(this->toFloat() + f.toFloat());
+	return r;
+}
+
+Fixed &Fixed::operator/(const Fixed &f) const
+{
+	Fixed r(this->toFloat() / f.toFloat());
+	return r;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return n;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	n = raw;
+}
+
+float Fixed::toFloat(void) const
+{
+	return n / 256.0;
+}
+
+int Fixed::toInt(void) const
+{
+	return n >> 8;
+}
+
+std::ostream &operator<<(std::ostream &os, const Fixed &f)
+{
+	os << f.toFloat();
+	return os;
+}
---
+++ CPP/02/ex02/Fixed.hpp
@@ -0,0 +1,37 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+private:
+	int n;
+	static const int bsize = 8;
+public:
+	Fixed();
+	Fixed(const int n);
+	Fixed(const float n);
+	Fixed(const Fixed &f);
+	Fixed &operator=(const Fixed &f);
+	bool &operator>(const Fixed &f) const;
+	bool &operator<(const Fixed &f) const;
+	bool &operator>=(const Fixed &f) const;
+	bool &operator<=(const Fixed &f) const;
+	bool &operator==(const Fixed &f) const;
+	bool &operator!=(const Fixed &f) const;
+	Fixed &operator+(const Fixed &f);
+	Fixed &operator-(const Fixed &f);
+	Fixed &operator*(const Fixed &f);
+	Fixed &operator/(const Fixed &f);
+	~Fixed();
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+	float toFloat(void) const;
+	int toInt(void) const;
+};
+
+std::ostream &operator<<(std::ostream &os, const Fixed &f);
+
+#endif
