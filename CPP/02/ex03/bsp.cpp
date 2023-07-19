357650b99ef1869c0b3c86d60e7e5cff1615af51
lnascari <lnascari@student.42firenze.it>
2023-07-19 12:19:21 +0200

    ok

--- CPP/02/ex03/bsp.cpp
+++ CPP/02/ex03/bsp.cpp
@@ -2,5 +2,15 @@
 
 bool bsp(Point const a, Point const b, Point const c, Point const point)
 {
-	
+	int count = 0;
+	if ((point.getY() < a.getY()) != (point.getY() < b.getY()) && 
+		point.getX() < a.getX() + (point.getY() - a.getY()) / (b.getY() - a.getY()) * (b.getX() - a.getX()))
+		count++;
+	if ((point.getY() < b.getY()) != (point.getY() < c.getY()) && 
+		point.getX() < b.getX() + (point.getY() - b.getY()) / (c.getY() - b.getY()) * (c.getX() - b.getX()))
+		count++;
+	if ((point.getY() < c.getY()) != (point.getY() < a.getY()) && 
+		point.getX() < c.getX() + (point.getY() - c.getY()) / (a.getY() - c.getY()) * (a.getX() - c.getX()))
+		count++;
+	return (count % 2 == 1);
 }
--- CPP/02/ex03/main.cpp
+++ CPP/02/ex03/main.cpp
@@ -7,7 +7,6 @@ int main()
 	Point a(1.5454, 3.234);
 	Point b(6.346, 1.4325);
 	Point c(5.8622, 6.2489);
-	Point p(6.123, 1.743);
-	//Point({6.123, 1.743})
+	Point p(6.123, 5.743);
 	std::cout << bsp(a, b, c, p) << std::endl;
 }
