d65c2ac03e53540c1bfa7da2a15e4d5da8fc5cf3
lnascari <lnascari@student.42firenze.it>
2023-10-12 11:44:00 +0200

    ok

--- CPP/06/ex00/ScalarConverter.hpp
+++ CPP/06/ex00/ScalarConverter.hpp
@@ -1,10 +1,13 @@
 #ifndef SCALARCONVERTER_HPP
 #define SCALARCONVERTER_HPP
 
+#include <iostream>
+#include <cstdlib>
+
 class ScalarConverter
 {
 public:
-	static void convert(char *);
+	static void convert(char *str);
 };
 
 #endif
