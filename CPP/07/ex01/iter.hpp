9a4ed3317cbc3af739294414705e848f7e34d763
lnascari <lnascari@student.42firenze.it>
2023-11-09 12:02:16 +0100

    ok

--- CPP/07/ex01/iter.hpp
+++ CPP/07/ex01/iter.hpp
@@ -1,6 +1,12 @@
 #ifndef ITER_HPP
 #define ITER_HPP
 
+template <typename T> void toupper(T &c)
+{
+	if (c >= 'a' && c <= 'z')
+		c -= 32;
+}
+
 template <typename T> void iter(T *arr, size_t len, void (*f)(T &el))
 {
 	for (size_t i = 0; i < len; i++)
--- CPP/07/ex01/main.cpp
+++ CPP/07/ex01/main.cpp
@@ -2,12 +2,6 @@
 #include <string>
 #include "iter.hpp"
 
-template <typename T> void toupper(T &c)
-{
-	if (c >= 'a' && c <= 'z')
-		c -= 32;
-}
-
 int main( void )
 {
 	char str[18] = {'	', 'g', 'i', 'a', 'm', 'b', 'a', 'T', 'T', 'i', 's', 't', 'A', ' ', ';' , '4', '2', 0};
