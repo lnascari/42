7e31563479af5cf73b28c86a13dd56241fa2b6e9
lnascari <lnascari@student.42firenze.it>
2023-11-09 15:56:45 +0100

    ok

--- CPP/07/ex02/Array.hpp
+++ CPP/07/ex02/Array.hpp
@@ -1,4 +1,57 @@
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
+template <typename T> class Array
+{
+private:
+	T *arr;
+	unsigned int n;
+public:
+	Array();
+	Array(unsigned int n);
+	Array(const Array &a);
+	Array &operator=(const Array &a);
+	~Array();
+	T& operator[](int i);
+	unsigned int size();
+};
+
 #endif
+
+template <typename T>
+inline Array<T>::Array()
+{
+}
+
+template <typename T>
+inline Array<T>::Array(unsigned int n)
+{
+}
+
+template <typename T>
+inline Array<T>::Array(const Array &a)
+{
+}
+
+template <typename T>
+inline Array &Array<T>::operator=(const Array &a)
+{
+	// TODO: insert return statement here
+}
+
+template <typename T>
+inline Array<T>::~Array()
+{
+}
+
+template <typename T>
+inline T &Array<T>::operator[](int i)
+{
+	// TODO: insert return statement here
+}
+
+template <typename T>
+inline unsigned int Array<T>::size()
+{
+	return 0;
+}
