f268df535cf77e3ffacddda7714dc31925bad670
lnascari <lnascari@student.42firenze.it>
2023-11-14 12:43:53 +0100

    ok

--- CPP/08/ex00/main.cpp
+++ CPP/08/ex00/main.cpp
@@ -9,6 +9,6 @@ int main()
 	std::list<int> l(arr, arr + sizeof(arr) / sizeof(arr[0]));
 	std::vector<int> v(arr, arr + sizeof(arr) / sizeof(arr[0]));
 	std::cout << easyfind(l, 4) << std::endl;
-	std::cout << easyfind(v, 1) << std::endl;
-	std::cout << easyfind(l, 9) << std::endl;
+	std::cout << easyfind(v, 2) << std::endl;
+	std::cout << easyfind(l, 0) << std::endl;
 }
