1296cbf486b5c85d42d4cd69b5e24807dff0d428
lnascari <lnascari@student.42firenze.it>
2023-11-14 15:30:03 +0100

    ok

---
+++ CPP/08/ex01/Span.cpp
@@ -0,0 +1,50 @@
+#include "Span.hpp"
+
+Span::Span(unsigned int N)
+{
+	this->N = N;
+	i = 0;
+}
+
+Span::Span(const Span &s)
+{
+	*this = s;
+}
+
+Span &Span::operator=(const Span &s)
+{
+	v = s.v;
+	N = s.N;
+	i = s.i;
+	return *this;
+}
+
+Span::~Span() {}
+
+void Span::addNumber(int n)
+{
+	if (i == N)
+		throw ArrayFullException();
+	v.push_back(n);
+	i++;
+}
+
+int Span::shortestSpan()
+{
+	if (v.size() < 2)
+		throw ArrayEmptyException();
+	int span = 0;
+	for (unsigned int i = 0; i < N - 1; i++)
+	{
+		if (!span || abs(v[i] - v[i + 1]) < span)
+			span = abs(v[i] - v[i + 1]);
+	}
+	return span;
+}
+
+int Span::longestSpan()
+{
+	if (v.size() < 2)
+		throw ArrayEmptyException();
+	return *std::max_element(v.begin(), v.end()) - *std::min_element(v.begin(), v.end());
+}
---
+++ CPP/08/ex01/Span.hpp
@@ -0,0 +1,38 @@
+#ifndef SPAN_HPP
+#define SPAN_HPP
+
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+class ArrayFullException : public std::exception {
+public:
+	const char *what() const throw() {
+		return "Array is full";
+	}
+};
+
+class ArrayEmptyException : public std::exception {
+public:
+	const char *what() const throw() {
+		return "Array is empty";
+	}
+};
+
+class Span
+{
+private:
+	std::vector<int> v;
+	unsigned int N;
+	unsigned int i;
+public:
+	Span(unsigned int N);
+	Span(const Span &s);
+	Span &operator=(const Span &s);
+	~Span();
+	void addNumber(int n);
+	int shortestSpan();
+	int longestSpan();
+};
+
+#endif
