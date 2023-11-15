fe532fded7c33ed47bbc3bb8ad8c8b402e875c46
lnascari <lnascari@student.42firenze.it>
2023-11-15 15:09:13 +0100

    ok

--- CPP/08/ex02/MutantStack.hpp
+++ CPP/08/ex02/MutantStack.hpp
@@ -2,53 +2,30 @@
 #define MUTANTSTACK_HPP
 
 #include <stack>
+#include <deque>
 
 template <typename T> class MutantStack : public std::stack<T>
 {
-private:
-	T *arr;
-	unsigned int n;
 public:
-	Array()
+	typedef typename std::deque<T>::iterator iterator;
+	MutantStack() {}
+	MutantStack(const MutantStack &m)
 	{
-		arr = new T();
-		n = 0;
+		*this = m;
 	}
-	Array(unsigned int n)
+	MutantStack &operator=(const MutantStack &m)
 	{
-		arr = new T[n];
-		this->n = n;
-	}
-	Array(const Array &a)
-	{
-		arr = new T[a.n];
-		n = a.n;
-		for (size_t i = 0; i < a.n; i++)
-			arr[i] = a.arr[i];
-	}
-	Array &operator=(const Array &a)
-	{
-		if(this == &a)
-			return *this;
-		delete[] arr;
-		arr = new T[a.n];
-		n = a.n;
-		for (size_t i = 0; i < a.n; i++)
-			arr[i] = a.arr[i];
+		std::stack<T>::operator=(m);
 		return *this;
 	}
-	~Array()
+	~MutantStack() {}
+	iterator begin()
 	{
-		delete[] arr;
+		return this->c.begin();
 	}
-	T& operator[](unsigned int i){
-		if (i >= n)
-			throw IndexOutOfBoundsException();
-		return arr[i];
-	}
-	unsigned int size()
+	iterator end()
 	{
-		return n;
+		return this->c.end();
 	}
 };
 
