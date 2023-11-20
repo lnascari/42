0a00ff8b9c46bf4c570dcdf34daccdf6c3cf056a
lnascari <lnascari@student.42firenze.it>
2023-11-20 14:17:44 +0100

    ok

--- CPP/09/ex00/main.cpp
+++ CPP/09/ex00/main.cpp
@@ -10,6 +10,16 @@ int main(int argc, char **argv)
 		f.open(argv[1]);
 		if (!f)
 			throw FileException();
+		std::string str;
+		std::getline(f, str);
+		if (str != "date | value") {
+			std::cerr << "Error: bad format" << std::endl;
+			f.close();
+		}
+		while (std::getline(f, str)) {
+			std::cout << str << std::endl;
+		}
+		
 		f.close();
 	}
 	catch(const std::exception& e)
