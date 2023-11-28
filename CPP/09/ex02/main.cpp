9ab36d503a7de1d135d6cfbaaf12f4abdbcb3a90
user <lorenzonascari@gmail.com>
2023-11-28 23:52:25 +0100

    ok

---
+++ CPP/09/ex02/main.cpp
@@ -0,0 +1,22 @@
+#include "PmergeMe.hpp"
+#include <iostream>
+#include <cstdlib>
+#include <cctype>
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2) {
+        std::cout << "Error" << std::endl;
+        return 1;
+    }
+    int arr[argc - 1];
+    for (int i = 1; i < argc; i++) {
+        for (int j = 0; argv[i][j]; j++) {
+            if (!isdigit(argv[i][j])) {
+                std::cout << "Error" << std::endl;
+                return 1;
+            }
+        }
+        arr[i - 1] = atoi(argv[i]);
+    }
+}
