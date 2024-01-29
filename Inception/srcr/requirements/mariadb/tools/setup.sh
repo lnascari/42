91a6256d762b6ddf239d4401904077bf30fcd48b
lnascari <lnascari@student.42firenze.it>
2024-01-29 14:47:37 +0100

    ok

--- Inception/srcr/requirements/mariadb/tools/setup.sh
+++ Inception/srcr/requirements/mariadb/tools/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 cat << EOF > /tmp/init.sql
+ALTER USER root@localhost IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 CREATE DATABASE wp;
 GRANT ALL PRIVILEGES ON wp.* TO ${MYSQL_USER}@localhost IDENTIFIED BY '${MYSQL_PASSWORD}';
 
