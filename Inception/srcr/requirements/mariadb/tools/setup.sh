6edc025b605c108c95ed3eae3bdb9930e6e96ec2
lnascari <lnascari@student.42firenze.it>
2024-01-30 14:45:04 +0100

    ok

--- Inception/srcr/requirements/mariadb/tools/setup.sh
+++ Inception/srcr/requirements/mariadb/tools/setup.sh
@@ -3,8 +3,7 @@
 cat << EOF > /tmp/init.sql
 ALTER USER root@localhost IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 CREATE DATABASE wp;
-GRANT ALL PRIVILEGES ON wp.* TO ${MYSQL_USER}@localhost IDENTIFIED BY '${MYSQL_PASSWORD}';
-
+GRANT ALL PRIVILEGES ON wp.* TO ${MYSQL_USER}@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 EOF
 
-mysqld_safe --init-file=/tmp/init.sql --user=root
+mysqld_safe --init-file=/tmp/init.sql --user=root --skip_networking=0
---
+++ Inception/srcr/requirements/wordpress/tools/config.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+cat << EOF > /www/wp-config.php
+<?php
+define( 'DB_NAME', 'wp' );
+define( 'DB_USER', '${MYSQL_USER}' );
+define( 'DB_PASSWORD', '${MYSQL_PASSWORD}' );
+define( 'DB_HOST', 'mariadb' );
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+define( 'WP_DEBUG', true );
+\$table_prefix = 'wp_';
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+require_once ABSPATH . 'wp-settings.php';
+EOF
